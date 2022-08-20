e0f6e3ebfd2ac16062adbafe7a0403e646dd0751
Bittersweet-girl <enatav1@gmail.com>
2022-08-20 16:17:59 +0300

    feat: add Header component and style

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,20 @@
+export interface IRender {
+  setRender(state: string): void;
+}
+
+export interface IProduct {
+  id: string,
+  'group': number,
+  'page': number,
+  'word': string,
+  'image': string,
+  'audio': string,
+  'audioMeaning': string,
+  'audioExample': string,
+  'textMeaning': string,
+  'textExample': string,
+  'transcription': string,
+  'wordTranslate': string,
+  'textMeaningTranslate': string,
+  'textExampleTranslate': string
+}
