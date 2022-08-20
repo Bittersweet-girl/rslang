17e88f2ef6036ea78014f3524165508318caa0ed
Bittersweet-girl <enatav1@gmail.com>
2022-08-21 00:20:07 +0300

    fix: change btns and footer styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@
 import React, { useState } from 'react';
 import Footer from './components/footer/footer';
 import Header from './components/header/Header';
+import Main from './components/main/main';
 
 function App() {
-  const [render, setRender] = useState<string>('main');
+  const [/* render */, setRender] = useState<string>('main');
 
   return (
     <div className="app">
       <Header setRender={setRender} />
+      <Main />
       <Footer />
     </div>
   );
