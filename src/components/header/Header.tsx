07a0ea0b78c3c44b94e559e8f0cd7d50a5b62b39
Bittersweet-girl <enatav1@gmail.com>
2022-08-21 23:36:01 +0300

    feat: add dropdown menu and fix some styles and names

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,7 +9,18 @@ export default function Header(props: IRender) {
       <div className="header-content">
         <button type="button" className="header__logo_btn" onClick={() => props.setRender('main')}> </button>
         <nav className="header-menu">
-          <button type="button" className="header-menu__button btn header-menu__button_dict" onClick={() => props.setRender('dict')}>Учебник</button>
+          <div className="header-menu-list">
+            <button type="button" className="header-menu__button btn header-menu__button_dict" onClick={() => props.setRender('dict')}>Учебник</button>
+            <div className="header-menu-list-content">
+              <button type="button" className="header-menu-list__button btn header-menu-list__button_one">Раздел первый</button>
+              <button type="button" className="header-menu-list__button btn header-menu-list__button_two">Раздел второй</button>
+              <button type="button" className="header-menu-list__button btn header-menu-list__button_three">Раздел третий</button>
+              <button type="button" className="header-menu-list__button btn header-menu-list__button_four">Раздел четвертый</button>
+              <button type="button" className="header-menu-list__button btn header-menu-list__button_five">Раздел пятый</button>
+              <button type="button" className="header-menu-list__button btn header-menu-list__button_six">Раздел шестой</button>
+              <button type="button" className="header-menu-list__button btn header-menu-list__button_difficult">Сложные слова</button>
+            </div>
+          </div>
           <button type="button" className="header-menu__button btn header-menu__button_audio" onClick={() => props.setRender('audio')}>Аудиовызов</button>
           <button type="button" className="header-menu__button btn header-menu__button_sprint" onClick={() => props.setRender('sprint')}>Спринт</button>
           <button type="button" className="header-menu__button btn header-menu__button_stat" onClick={() => props.setRender('statistic')}>Статистика</button>
